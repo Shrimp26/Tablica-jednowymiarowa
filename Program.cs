aea0180b4e5d9b3f27a8b24fd5704947d8e1f785
julia <julia@LAPTOP-ILMLJSM->
2024-02-10 01:46:08 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+//Napisz program, który w 10-elementowej tablicy jednowymiarowej o nazwie tablica1 umieszcza liczby z przedziału od 0 do 9.
+
+class TablicaJednowymiarowa
+{
+    static void Main(string[] args)
+    {
+        const int rozmiar_tablicy = 10;
+        int[] tablica1 = new int[rozmiar_tablicy];
+
+        Random rand = new Random();
+
+        for (int i = 0; i < rozmiar_tablicy; i++)
+        {
+            tablica1[i] = rand.Next(10);
+        }
+
+        Console.WriteLine("Zawartość tablicy: ");
+        foreach (int liczba in tablica1)
+        {
+            Console.WriteLine(liczba + " ");
+        }
+    }
+}
